59992abfb44bc4cb7f236847c14d24121e8687fc
yarden lebel <yardenlebel1@gmail.com>
2024-03-31 15:45:14 +0300

    start
    base web

---
+++ web/prisma/migrations/20240331120609_add_products/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `shop` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `isOnline` BOOLEAN NOT NULL DEFAULT false,
+    `scope` VARCHAR(191) NULL,
+    `expires` DATETIME(3) NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `userId` BIGINT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
